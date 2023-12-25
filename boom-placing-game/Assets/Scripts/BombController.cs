c55e61a1ae7fa78f8891d78d0164232072d8ce08
duogglong <nduonglong02@gmail.com>
2023-12-25 17:12:50 +0700

    bomb explosion, change scene SampleScene into Solo1vs1Scene

--- boom-placing-game/Assets/Scripts/BombController.cs
+++ boom-placing-game/Assets/Scripts/BombController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,10 +14,10 @@ public class BombController : MonoBehaviour
     private int bombsRemaining;
 
     [Header("Explosion")]
-    //public Explosion explosionPrefab;
+    public Explosion explosionPrefab;
     public LayerMask explosionLayerMask;
     public float explosionDuration = 1f;
-    public int explosionRadius = 1;
+    public const int explosionRadius = 2;
 
     //[Header("Destructible")]
     //public Tilemap destructibleTiles;
@@ -53,19 +54,56 @@ public class BombController : MonoBehaviour
         position.x = Mathf.Round(position.x);
         position.y = Mathf.Round(position.y);
 
-        //Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        //explosion.SetActiveRenderer(explosion.start);
-        //explosion.DestroyAfter(explosionDuration);
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
 
-        //Explode(position, Vector2.up, explosionRadius);
-        //Explode(position, Vector2.down, explosionRadius);
-        //Explode(position, Vector2.left, explosionRadius);
-        //Explode(position, Vector2.right, explosionRadius);
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
 
-        //...
         Destroy(bomb);
         bombsRemaining++;
     }
 
-   
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+        Debug.Log("Log 1: " + position + " " + direction);
+        position += direction;
+        Debug.Log("Log 2: " + position);
+
+        //if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        //{
+        //    ClearDestructible(position);
+        //    return;
+        //}
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        Debug.Log(length);
+        Debug.Log(1 > 1 );
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        //explosion.SetActiveRenderer(explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, length - 1);
+    }
+
+    //private void ClearDestructible(Vector2 position)
+    //{
+    //    Vector3Int cell = destructibleTiles.WorldToCell(position);
+    //    TileBase tile = destructibleTiles.GetTile(cell);
+
+    //    if (tile != null)
+    //    {
+    //        Instantiate(destructiblePrefab, position, Quaternion.identity);
+    //        destructibleTiles.SetTile(cell, null);
+    //    }
+    //}
+
 }
