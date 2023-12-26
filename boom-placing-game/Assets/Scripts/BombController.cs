172d07a770f80f5f22038f36aa9d6a572684e75f
CaoBaLoi <2051063605@e.tlu.edu.vn>
2023-12-27 00:36:46 +0700

    destroy brick

--- boom-placing-game/Assets/Scripts/BombController.cs
+++ boom-placing-game/Assets/Scripts/BombController.cs
@@ -17,11 +17,11 @@ public class BombController : MonoBehaviour
     public Explosion explosionPrefab;
     public LayerMask explosionLayerMask;
     public float explosionDuration = 1f;
-    public const int explosionRadius = 2;
+    public const int explosionRadius = 1;
 
-    //[Header("Destructible")]
-    //public Tilemap destructibleTiles;
-    //public Destructible destructiblePrefab;
+    [Header("Destructible")]
+    public Tilemap destructibleTiles;
+    public Destructible destructiblePrefab;
 
     private void OnEnable()
     {
@@ -77,11 +77,11 @@ public class BombController : MonoBehaviour
         position += direction;
         Debug.Log("Log 2: " + position);
 
-        //if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
-        //{
-        //    ClearDestructible(position);
-        //    return;
-        //}
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        {
+           ClearDestructible(position);
+           return;
+        }
 
         Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
         Debug.Log(length);
@@ -94,16 +94,16 @@ public class BombController : MonoBehaviour
         Explode(position, direction, length - 1);
     }
 
-    //private void ClearDestructible(Vector2 position)
-    //{
-    //    Vector3Int cell = destructibleTiles.WorldToCell(position);
-    //    TileBase tile = destructibleTiles.GetTile(cell);
+    private void ClearDestructible(Vector2 position)
+    {
+       Vector3Int cell = destructibleTiles.WorldToCell(position);
+       TileBase tile = destructibleTiles.GetTile(cell);
 
-    //    if (tile != null)
-    //    {
-    //        Instantiate(destructiblePrefab, position, Quaternion.identity);
-    //        destructibleTiles.SetTile(cell, null);
-    //    }
-    //}
+       if (tile != null)
+       {
+           Instantiate(destructiblePrefab, position, Quaternion.identity);
+           destructibleTiles.SetTile(cell, null);
+       }
+    }
 
 }
